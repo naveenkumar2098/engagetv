353c9c8f6e50fff819e3e8764e71f03594648fe6
Naveen <kumarn.na20@gmail.com>
2020-10-19 15:56:43 +0530

    Movie Component Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { useState ,useEffect} from 'react';
+import { KEY } from './backend';
+import Movie from './components/Movie';
+
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${KEY}&page=1`;
+
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${KEY}&query=`;
 
 function App() {
-  return <div>Hello Naveen</div>;
+
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+        fetch(FEATURED_API)
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data)
+                setMovies(data.results);
+            });
+    }, []);
+  
+    return(
+        <div className="movie-container">
+            { movies.length > 0 &&
+                movies.map((movie) => <Movie key={movie.id} {...movie} />
+            )}
+        </div>
+    );  
 }
 
 export default App;
