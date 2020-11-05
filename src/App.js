660ba9afe935e93ada18805cebdb624960b8ea49
Naveen <kumarn.na20@gmail.com>
2020-11-05 20:22:43 +0530

    Added Search bar

--- src/App.js
+++ src/App.js
@@ -9,22 +9,54 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${KEY}&qu
 function App() {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
     
     useEffect(() => {
-        fetch(FEATURED_API)
+        getMovies(FEATURED_API);
+    }, []);
+
+    const getMovies = (API) => {
+        fetch(API)
             .then((res) => res.json())
             .then((data) => {
               console.log(data)
                 setMovies(data.results);
             });
-    }, []);
+    }
+
+    const handleOnSubmit = (event) => {
+        event.preventDefault();
+        
+        if(searchTerm){
+            getMovies(SEARCH_API + searchTerm)
+            setSearchTerm("");
+        }
+        
+    };
+
+    const handleOnChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
   
     return(
-        <div className="movie-container">
-            { movies.length > 0 &&
-                movies.map((movie) => <Movie key={movie.id} {...movie} />
-            )}
-        </div>
+        <>
+            <div className="search-container">
+                <form onSubmit={handleOnSubmit}>
+                    <input 
+                        className="search" 
+                        type="search" 
+                        placeholder="Search..." 
+                        value={searchTerm}
+                        onChange={handleOnChange}
+                    />
+                </form>
+            </div>
+            <div className="movie-container">
+                { movies.length > 0 &&
+                    movies.map((movie) => <Movie key={movie.id} {...movie} />
+                )}
+            </div>
+        </>
     );  
 }
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+    if(vote >= 8){
+        return "green";
+    }else if(vote >= 6){
+        return "orange";
+    }else{
+        return "red";
+    }
+};
+
 const Movie = ({overview, title, poster_path, vote_average}) => (
     <div className="movie">
-        <img src={IMG_API + poster_path} alt={title} />
+        <img src={poster_path ? 
+            IMG_API + poster_path
+            : "https://images.pexels.com/photos/1117132/pexels-photo-1117132.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+            } alt={title} 
+        />
         <div className="movie-info">
             <h3>{title}</h3>
-            <span>{vote_average}</span>
+            <span className={`tag ${setVoteClass(vote_average)}`} >{vote_average}</span>
         </div>
         <div className="movie-hover">
             <h2>Overview:</h2>
